14bfa7ddfb184b8ef9eccfe51d0f38340ca93b04
Anushka <bhardwajanushka180@gmail.com>
2023-09-23 15:02:27 +0530

    basic blog page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
         </a>
+
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+
+      <div className="post">
+        <div className="image">
+          {" "}
+          <img
+            src="https://images.unsplash.com/photo-1581798269145-7512508289b9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhbmR5fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60"
+            alt="imgentry"
+          />{" "}
+        </div>
+
+        <div className="postContent">
+          <h2>The History of Candy: A Sweet Journey Through Time</h2>
+          <p className="info">
+            <span className="author">Anushka</span>
+            <time>2023-07-31</time>
+          </p>
+          <p className="summary">
+            Explore the fascinating history of candy, from its ancient origins
+            to modern-day favorites. Share fun facts and trivia about the
+            evolution of candy-making techniques and popular treats.
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1581798269145-7512508289b9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhbmR5fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60"
+            alt="imgentry"
+          />
+        </div>
+
+        <div className="postContent">
+          <h2>The History of Candy: A Sweet Journey Through Time</h2>
+          <p className="info">
+            <span className="author">Anushka</span>
+            <time>2023-07-31</time>
+          </p>
+          <p className="summary">
+            Explore the fascinating history of candy, from its ancient origins
+            to modern-day favorites. Share fun facts and trivia about the
+            evolution of candy-making techniques and popular treats.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
